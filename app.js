2e8c8bebebf7788c9b6e726d0a73683382a0aa50
Unknown <kl0231582@otc.edu>
2017-11-07 04:22:25 -0600

    stuck

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ $(function() {
     var app = new Vue({
         el: '#app',
         data: {
+            id: " ",
+            question: " ",
+            results: [],
+            category: " ",
 
+
+
+        },
+        methods: {
+            trivia() {
+                this.$http.get('https://opentdb.com/api.php?amount=1')
+                    .then(resp => {
+                        this.results = resp.body
+                        this.find()
+                    })
+            },
+            find() {
+                var title1 = results[0].category
+            }
 
         }
     })
